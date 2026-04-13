c04c3f1ed21bd289be27f56634e4581f8eddea9d
Quantum <zerotheking152@gmail.com>
2026-04-13 13:17:23 +0700

    Update Scratch.lua

--- Scratch.lua
+++ Scratch.lua
@@ -2,7 +2,7 @@ local ConfigMap = {
 
     -- Fish it
     [121864768012064] = {
-        url = "link script"
+        url = "link script"  -- ganti dengan link asli kalau sudah ada
     },
 
     -- Violence Distrik
@@ -13,13 +13,18 @@ local ConfigMap = {
     -- Grow a garden
     [126884695634066] = {
         url = "https://raw.githubusercontent.com/zerotheking152-png/QuantumGAG/main/QHCommunity.lua"
+    },
+
+    -- Sawah Indo
+    [83369512629707] = {
+        url = "https://raw.githubusercontent.com/zerotheking152-png/QuantumSAWAH/refs/heads/main/Community.lua"
     }
 }
 
 local config = ConfigMap[game.PlaceId]
 
-if config then
-    print("Loading:", game.PlaceId)
+if config and config.url then
+    print("Loading script for PlaceId:", game.PlaceId)
     pcall(function()
         loadstring(game:HttpGet(config.url))()
     end)
