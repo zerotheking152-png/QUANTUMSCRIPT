12b638dbbbc6c746e57f430313649db32826a2f2
Quantum <zerotheking152@gmail.com>
2026-05-08 21:32:55 +0700

    Update URL in ConfigMap for specific ID

--- Scratch.lua
+++ Scratch.lua
@@ -1,6 +1,6 @@
 local ConfigMap = {
     [121864768012064] = {
-        url = "https://pastefy.app/eaAoL6iD/raw"
+        url = "https://pastefy.app/jwbWh4kL/raw"
     },
     [93978595733734] = {
         url = "https://pastefy.app/F0AvOIIh/raw"
